33b650e1852db948031f3bfec161da4ee1b23be9
Eren Tepecik <erntpck@gmail.com>
2023-06-24 23:21:44 +0300

    VievComponent ve PartialView lar kullanıldı.

---
+++ AspNetMvcBlog/App.Web.Mvc/App.Web.Mvc/Models/ProductItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Web.Mvc.Models
+{
+    public class ProductItemModel
+    {
+        public string Title { get; set; }
+        public int StarCount { get; set; }
+        public decimal OldPrice { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsSale { get; set; }
+    }
+}
